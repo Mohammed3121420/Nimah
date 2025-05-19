de8064d7d4abf25b9d169cac991e2e8a1502764b
Mohammed3121420 <m.mohammed3121420@gmail.com>
2025-05-19 22:35:49 +0300

    ProductDetail Screen

---
+++ lib/features/order/presentation/screen/product_detail_page.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailPage extends StatefulWidget {
+  final String title;
+  final String image;
+  final String price;
+
+  const ProductDetailPage({
+    super.key,
+    required this.title,
+    required this.image,
+    required this.price,
+  });
+
+  @override
+  State<ProductDetailPage> createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState extends State<ProductDetailPage> {
+  int quantity = 1;
+
+  final List<Map<String, dynamic>> addOns = [
+    {"name": "Vegan Mayo", "price": 2.99, "selected": false},
+    {"name": "Sliced Tomatoes", "price": 1.99, "selected": false},
+    {"name": "Whole Wheat Buns", "price": 2.00, "selected": false},
+    {"name": "Bell Peppers", "price": 1.25, "selected": false},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.deepOrange,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+          ),
+          onPressed: () {
+            //! راح يتغير هذا
+            List<Map<String, dynamic>> selectedAddOns =
+                addOns.where((item) => item["selected"] == true).toList();
+
+            print("Selected Add-Ons: $selectedAddOns");
+            print("Quantity: $quantity");
+            print("Main Product: ${widget.title}, Price: ${widget.price}");
+          },
+          icon: const Icon(Icons.shopping_cart_outlined, size: 24),
+          label: const Text("Add to Cart", style: TextStyle(fontSize: 18)),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header
+            Container(
+              height: 150,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.orange[700],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        'Restaurant name',
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  //* Product image
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.asset(
+                      //! صوره الطلب نفسه
+                      widget.image,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: 220,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        //! سعر الطلب نفسه
+                        widget.price,
+                        style: const TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepOrange,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              if (quantity > 1) {
+                                setState(() => quantity--);
+                              }
+                            },
+                            icon: const Icon(Icons.remove_circle_outline),
+                          ),
+                          Text(
+                            quantity.toString(),
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                          IconButton(
+                            onPressed: () => setState(() => quantity++),
+                            icon: const Icon(Icons.add_circle_outline),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 12),
+
+                  const Text(
+                    //! وصف الطلب
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  const SizedBox(height: 24),
+
+                  const Text(
+                    "Add on ingredients",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  const SizedBox(height: 12),
+                  ..._buildAddOns(),
+                  const SizedBox(height: 80),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildAddOns() {
+    return addOns.map((item) {
+      return Container(
+        margin: const EdgeInsets.symmetric(vertical: 6),
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.white),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                item["name"],
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Text(
+                  "SR${item["price"].toStringAsFixed(2)}",
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Transform.scale(
+                  scale: 1.2,
+                  child: Checkbox(
+                    shape: const CircleBorder(),
+                    activeColor: Colors.deepOrange,
+                    value: item["selected"],
+                    onChanged: (value) {
+                      setState(() {
+                        item["selected"] = value!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+}
